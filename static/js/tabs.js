04ae126fe915476c0f3f0dda5e1c20b9a87e9e3a
Henry Ventura <hvent90@gmail.com>
2018-07-18 09:23:27 -0700

    exporters.md. Reimplement tabs, fixes #16

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -6,10 +6,38 @@ function initializeTabs(element) {
       });
   }
 
-  childElements(element.querySelector('.tabs-body'))
-    .forEach(function(node, i) {
-      node.id = i;
-    });
+  var tabs = childElements(element.querySelector('ul.tabs'));
+  var tabContents = childElements(element.querySelector('span.tabs-body'));
 
-  M.Tabs.init(element.querySelector('.tabs'));
+  tabs.forEach(function(tabElement, i) {
+    tabElement.classList.add('highlight');
+    tabElement.dataset.id = i;
+
+    tabElement.addEventListener('click', function() {
+      tabs.forEach(function(tabElement) {
+        tabElement.classList.remove('active');
+      });
+
+      tabContents.forEach(function(tabContent) {
+        tabContent.classList.remove('active');
+        tabContent.classList.add('hide');
+
+        if (tabElement.dataset.id === tabContent.dataset.id) {
+          tabContent.classList.add('active');
+          tabContent.classList.remove('hide');
+        }
+      });
+
+      tabElement.classList.add('active');
+    });
+  });
+
+  tabContents.forEach(function(tabContent, i) {
+    tabContent.classList.add('highlight', 'hide');
+    tabContent.dataset.id = i;
+  });
+
+  tabs[0].classList.add('active');
+  tabContents[0].classList.add('active');
+  tabContents[0].classList.remove('hide');
 }
